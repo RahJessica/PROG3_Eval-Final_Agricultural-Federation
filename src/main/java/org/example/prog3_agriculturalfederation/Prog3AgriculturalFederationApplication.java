4010b80136f6ee3bcdf4aee077b709af2f39f82c
RahJess <jessicakantotiana@gmail.com>
2026-04-20 12:35:42 +0200

    Initial commit

---
+++ src/main/java/org/example/prog3_agriculturalfederation/Prog3AgriculturalFederationApplication.java
@@ -0,0 +1,13 @@
+package org.example.prog3_agriculturalfederation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prog3AgriculturalFederationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prog3AgriculturalFederationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/prog3_agriculturalfederation/Prog3AgriculturalFederationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.prog3_agriculturalfederation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prog3AgriculturalFederationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
